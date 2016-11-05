075c06a6360d8b88745e3bffd4883beead36c59b
Gabi Nagy <gabrian.nagy@gmail.com>
2016-11-05 21:27:23 +0200

    Add orders_script

--- config_example.py
+++ config_example.py
@@ -22,3 +22,20 @@ NEXMO = {
 }
 
 TEST = True
+
+orders_script = """
+var orders = [];
+var my_name = document.querySelector('#h-profilename').textContent;
+var name_tags = Array.prototype.slice.call(document.querySelectorAll('.container-white-rounded .header-left p'));
+var price_tags = Array.prototype.slice.call(document.querySelectorAll('.container-white-rounded .summary-total .value'));
+if (name_tags.length > price_tags.length) {
+    name_tags.splice(0, 1);
+}
+for (var i = 0; i < name_tags.length; i++) {
+    orders.push({
+        name: name_tags[i].textContent.replace('Selecțiile mele', my_name).trim(),
+        price: price_tags[i].textContent.trim(),
+    });
+}
+return orders;
+"""
